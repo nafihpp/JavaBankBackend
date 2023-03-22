5a1af7aa7137ad79c291e6c04e3e1defa654f7f6
nafihpp <nafihpp@gmail.com>
2023-03-22 21:35:23 +0530

    First

---
+++ Customer Microservice/src/main/java/com/javatech/bankingBackendJava/BankingBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.javatech.bankingBackendJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingBackendJavaApplication.class, args);
+	}
+
+}
